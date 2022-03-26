5168cafbd5815b3574c0e69773941eb7dc3a296c
Rewriter00x <colossus00x@icloud.com>
2022-03-26 18:12:01 +0200

    Build files updates

---
+++ Build/Intermediates.noindex/MyDLAsteroids.build/Debug/MyDLAsteroids.build/Objects-normal/arm64/MyFramework.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyFramework.cpp \
+  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyFramework.h \
+  /Users/rewriter/C++/MyDLAsteroids/External/Include/Framework.h
---
+++ Build/Intermediates.noindex/MyDLAsteroids.build/Debug/MyDLAsteroids.build/Objects-normal/arm64/main.d
@@ -0,0 +1,4 @@
+dependencies: /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/main.cpp \
+  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyDLAsteroidsFramework.h \
+  /Users/rewriter/C++/MyDLAsteroids/External/Include/Framework.h \
+  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyFramework.h
