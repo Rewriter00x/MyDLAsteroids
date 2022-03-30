1477bbe8802dae55b68f847bd755bf2bfcfc0509
Rewriter00x <colossus00x@icloud.com>
2022-03-30 15:23:41 +0300

    Removed build files

--- Build/Intermediates.noindex/MyDLAsteroids.build/Debug/MyDLAsteroids.build/Objects-normal/arm64/MyDLAsteroidsFramework.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyDLAsteroidsFramework.cpp \
-  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyDLAsteroidsFramework.h \
-  /Users/rewriter/C++/MyDLAsteroids/External/Include/Framework.h
--- Build/Intermediates.noindex/MyDLAsteroids.build/Debug/MyDLAsteroids.build/Objects-normal/arm64/MyFramework.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyFramework.cpp \
-  /Users/rewriter/C++/MyDLAsteroids/MyDLAsteroids/MyFramework.h \
-  /Users/rewriter/C++/MyDLAsteroids/External/Include/Framework.h
